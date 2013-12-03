22ed1eb2a1445471300255766e77e40a7057ff9c
Andrea Zito <zito.andrea@gmail.com>
2013-12-03 20:12:50 +0100

    Added shortcuts for multi-term mode

--- config/multi-term-conf.el
+++ config/multi-term-conf.el
@@ -9,3 +9,7 @@
   (term-send-raw-string "\e"))
 
 (add-to-list 'term-bind-key-alist '("C-c C-e" . term-send-esc))
+(add-to-list 'term-bind-key-alist '("C-c C-j" . term-line-mode))
+(add-to-list 'term-bind-key-alist '("C-c C-k" . term-char-mode))
+(add-to-list 'term-bind-key-alist '("C-c C-p" . multi-term-prev))
+(add-to-list 'term-bind-key-alist '("C-c C-n" . multi-term-next))
